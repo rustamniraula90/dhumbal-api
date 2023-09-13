9fba44d95c7ab7944c2809b63a349555335172bc
Rustam Niraula <niraula.rustam90@gmail.com>
2023-09-13 09:44:11 +0545

    initial commit

---
+++ src/test/java/com/fyp/dhumbal/DhumbalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyp.dhumbal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhumbalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
